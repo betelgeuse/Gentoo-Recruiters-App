74adedf571f8e0356727ae857aabd2fc2e8dfdfd
Joachim Filip Ignacy Bartosik <jbartosik@gmail.com>
2010-06-25 16:55:24 +0200

    Use hobo plugin, not Hobo gem (until next Hobo gem release).

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,6 @@ RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'hobo'
   config.gem 'hobo_openid'
 
   config.time_zone = 'UTC'

f72b3e4ec07e98f5218d4cbf405bf9c57485e1b3
Joachim Filip Ignacy Bartosik <jbartosik@gmail.com>
2010-07-07 11:10:38 +0200

    Allow users to suggest questions

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100625160008) do
+ActiveRecord::Schema.define(:version => 20100707090607) do
 
   create_table "answers", :force => true do |t|
     t.text     "content"
@@ -51,16 +51,25 @@ ActiveRecord::Schema.define(:version => 20100625160008) do
     t.datetime "updated_at"
   end
 
+  create_table "question_groups", :force => true do |t|
+    t.string   "name",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "questions", :force => true do |t|
     t.string   "title"
     t.text     "content"
     t.string   "documentation"
+    t.boolean  "approved",             :default => false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
     t.integer  "question_category_id"
   end
 
   add_index "questions", ["question_category_id"], :name => "index_questions_on_question_category_id"
+  add_index "questions", ["user_id"], :name => "index_questions_on_user_id"
 
   create_table "user_categories", :force => true do |t|
     t.datetime "created_at"
@@ -72,7 +81,19 @@ ActiveRecord::Schema.define(:version => 20100625160008) do
   add_index "user_categories", ["question_category_id"], :name => "index_user_categories_on_question_category_id"
   add_index "user_categories", ["user_id"], :name => "index_user_categories_on_user_id"
 
+  create_table "user_question_groups", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "question_id"
+  end
+
+  add_index "user_question_groups", ["question_id"], :name => "index_user_question_groups_on_question_id"
+  add_index "user_question_groups", ["user_id"], :name => "index_user_question_groups_on_user_id"
+
   create_table "users", :force => true do |t|
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
     t.string   "remember_token"
     t.datetime "remember_token_expires_at"
     t.string   "name"
@@ -80,14 +101,12 @@ ActiveRecord::Schema.define(:version => 20100625160008) do
     t.boolean  "administrator",                           :default => false
     t.string   "role",                                    :default => "recruit"
     t.string   "nick"
+    t.text     "contributions"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "mentor_id"
     t.string   "state",                                   :default => "active"
     t.datetime "key_timestamp"
-    t.string   "crypted_password",          :limit => 40
-    t.string   "salt",                      :limit => 40
-    t.text     "contributions"
   end
 
   add_index "users", ["mentor_id"], :name => "index_users_on_mentor_id"
